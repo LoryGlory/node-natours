47b9bd9138609c5cdbadcc157a81b5040218fbd4
Laura Roganovic <laura.roganovic@googlemail.com>
2023-02-25 12:18:12 +0100

    add env variables & dotenv dependency

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
+const dotenv = require('dotenv');
 const app = require('./app');
 const port = 3000;
+
+dotenv.config({ path: './config.env' });
+
+// get environment where we're in
+console.log(process.env);
 
 //start server
 app.listen(port, () => {
