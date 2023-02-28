ab1a2259dd825319f7e4727eed40dbca65dfdbca
Laura Roganovic <laura.roganovic@googlemail.com>
2023-02-28 14:31:22 +0100

    add async function to create new tour

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,38 +1,4 @@
-// const fs = require('fs');
 const Tour = require('../models/tourModel');
-
-// read data from file
-// const tours = JSON.parse(
-//   fs.readFileSync(
-//     `././dev-data/data/tours-simple.json`,
-//     'utf-8'
-//   )
-// );
-
-// middleware to check id of tour
-// exports.checkID = (req, res, next, val) => {
-//   console.log(`Tour id is: ${val}`);
-//
-//   if (req.params.id * 1 > tours.length) {
-//     return res.status(404).json({
-//       status: 'fail',
-//       message: 'Invalid ID',
-//     });
-//   }
-//
-//   next();
-// };
-
-// middleware to check name and price
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
-      status: 'fail',
-      message: 'Missing name or price!',
-    });
-  }
-  next();
-};
 
 // get all tours function
 exports.getAllTours = (req, res) => {
@@ -64,13 +30,22 @@ exports.getTour = (req, res) => {
 };
 
 // function to create Tour
-exports.createTour = (req, res) => {
-  res.status(201).json({
-    status: 'success',
-    // data: {
-    //   tour: newTour,
-    // },
-  });
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err.errmsg,
+    });
+  }
 };
 
 exports.updateTour = (req, res) => {
