80c140c2d3c7096faade5ea17f3073573c0c9699
Laura Roganovic <laura.roganovic@googlemail.com>
2023-02-28 10:38:52 +0100

    add mongoose connection

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose.set('strictQuery', false);
+mongoose.connect(DB, () => {
+  console.log(
+    `MongoDB Connect (0-disconnected; 1-connected; 2-connecting; 3-disconnecting; 4-invalid credentials) STATUS  --> ${mongoose.connection.readyState}`
+  );
+});
 
 const port = process.env.PORT || 3000;
 
