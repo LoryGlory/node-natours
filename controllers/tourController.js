227082b328fe99463216e4e66e1e7b2fd5bea4f5
Laura Roganovic <laura.roganovic@googlemail.com>
2023-03-01 12:41:31 +0100

    add pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -43,6 +43,22 @@ exports.getAllTours = async (req, res) => {
       query = query.select('-__v');
     }
 
+    // pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      // return number of documents
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) {
+        throw new Error('This page does not exist!');
+      }
+    }
+
+    // save final query results to tours variable
     const tours = await query;
 
     res.status(200).json({
@@ -56,7 +72,7 @@ exports.getAllTours = async (req, res) => {
   } catch (err) {
     res.status(404).json({
       status: 'fail',
-      message: err.errmsg,
+      message: err.message,
     });
   }
 };
