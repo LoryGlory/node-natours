8f0c9b0d64fc6c62c1c7e294a45a4eb6971ec7d5
Laura Roganovic <laura.roganovic@googlemail.com>
2023-03-02 16:10:45 +0100

    add bcrypt to hash password and validation function to schema

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,7 @@
 // user schema & model
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -28,7 +29,24 @@ const userSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, 'Please confirm your password.'],
+    validate: {
+      // this only works on save & create!
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not the same.',
+    },
   },
+});
+
+userSchema.pre('save', async function (next) {
+  // only run this function if password was actually modified
+  if (!this.isModified('password')) return next();
+
+  // set current password to the encrypted password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined; // set confirmed password to undefined, so it doesn't persist in db
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
