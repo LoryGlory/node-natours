db77bb454223006dd8ab00e11c433fb57c77d6a5
Laura Roganovic <laura.roganovic@googlemail.com>
2023-03-02 12:22:46 +0100

    add error handling for nonexistent routes and middleware error handler

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const morgan = require('morgan');
 const app = express();
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 
 // middleware to modify incoming request data
 // http request logger running only when in dev environment
@@ -27,5 +29,19 @@ app.use((req, res, next) => {
 // middleware to mount routers
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+// handle nonexistent routes
+app.all('*', (req, res, next) => {
+  // add error to next function, passing all other middlewares and going straight to the error
+  next(
+    new AppError(
+      `Can't find ${req.originalUrl} on this server!`,
+      404
+    )
+  );
+});
+
+// middleware for error handling
+app.use(globalErrorHandler);
 
 module.exports = app;
