955875cb812ba6f2c7203b7f51c9e3fc55a64b07
Laura Roganovic <laura.roganovic@googlemail.com>
2023-03-03 16:19:36 +0100

    add function to get all users, returning length of results and user data

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,19 @@
+const User = require('../models/userModel');
+const catchAsync = require('../utils/catchAsync');
+
 // functions for users
-exports.getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined',
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+  const users = await User.find();
+
+  res.status(200).json({
+    requestedAt: req.requestTime,
+    status: 'success',
+    results: users.length,
+    data: {
+      users,
+    },
   });
-};
+});
 
 exports.getUser = (req, res) => {
   res.status(500).json({
