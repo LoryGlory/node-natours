da047cb33c56cc5c09f047087664fb1539796629
Laura Roganovic <laura.roganovic@googlemail.com>
2023-03-03 20:49:09 +0100

    add token checks for access to protected route

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,4 @@
+const { promisify } = require('util');
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
@@ -11,21 +12,19 @@ const signToken = (id) => {
 
 exports.signup = catchAsync(async (req, res, next) => {
   // define data stored in User object (cannot add roles etc.)
-  const newUser = await User.create({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-    passwordConfirm: req.body.passwordConfirm,
-  });
-
-  // create jsonwebtoken token for user login
+  const newUser = await User.create(req.body);
   const token = signToken(newUser._id);
 
   res.status(201).json({
     status: 'success',
     token,
     data: {
-      user: newUser,
+      user: {
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        passwordConfirm: req.body.passwordConfirm,
+      },
     },
   });
 });
@@ -67,4 +66,56 @@ exports.login = catchAsync(async (req, res, next) => {
     status: 'success',
     token,
   });
+});
+
+exports.protect = catchAsync(async (req, res, next) => {
+  // get token, check if it exists
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+
+  if (!token) {
+    return next(
+      new AppError(
+        'You are not logged in! Please log in to get access.',
+        401
+      )
+    );
+  }
+
+  // verify token
+  const decoded = await promisify(jwt.verify)(
+    token,
+    process.env.JWT_SECRET
+  );
+
+  // check if user still exists
+  const freshUser = await User.findById(decoded.id);
+
+  if (!freshUser) {
+    return next(
+      new AppError(
+        'The user belonging to this token does no longer exist.'
+      ),
+      401
+    );
+  }
+
+  // check if user changed password after the token was issued
+  if (freshUser.changedPasswordAfter(decoded.iat)) {
+    return next(
+      new AppError(
+        'User recently changed password! Please log in again!',
+        401
+      )
+    );
+  }
+
+  // grant access to protected route
+  req.user = freshUser;
+  next();
 });
