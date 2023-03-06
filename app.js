704aa37b651f452dd028531aad4be6bfa7db0d3b
Laura Roganovic <laura.roganovic@googlemail.com>
2023-03-06 14:24:35 +0100

    add data sanitization to protect against NoSQL query injections and Cross-Site-Scripting

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const app = express();
 const tourRouter = require('./routes/tourRoutes');
@@ -29,6 +31,12 @@ app.use('/api', limiter);
 
 // body parser (read data from body into req.body)
 app.use(express.json({ limit: '10kb' }));
+
+// data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// data sanitization against cross-site-scripting (xss)
+app.use(xss());
 
 // use middleware to serve static files, setting public as root
 app.use(express.static(`${__dirname}/public`));
