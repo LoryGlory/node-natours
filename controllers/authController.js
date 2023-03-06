b7a2e06853fcbd12b10958f13bd68bfaeaada6d6
Laura Roganovic <laura.roganovic@googlemail.com>
2023-03-06 13:47:45 +0100

    add response method to send JWT as a cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,10 +12,30 @@ const signToken = (id) => {
   });
 };
 
-// function for
+// function for creating and sending JWT
 const createSendToken = (user, statusCode, res) => {
   // log user in (send JWT)
   const token = signToken(user._id);
+
+  // set expiration date to now + 10 minutes (in milliseconds)
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() +
+        process.env.JWT_COOKIE_EXPIRES_IN *
+          24 *
+          60 *
+          60 *
+          1000
+    ),
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+  };
+
+  // remove password from output
+  user.password = undefined;
+
+  // send cookie & cookieOptions
+  res.cookie('jwt', token, cookieOptions);
 
   res.status(statusCode).json({
     status: 'success',
