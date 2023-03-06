aa1e736524a5fc5c46ab70a96b5c8968d0c632d9
Laura Roganovic <laura.roganovic@googlemail.com>
2023-03-06 16:17:57 +0100

    change file to read different tour data file

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -18,9 +18,8 @@ mongoose.connect(DB, () => {
 });
 
 // read json file
-
 const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+  fs.readFileSync(`${__dirname}/tours.json`, 'utf-8')
 );
 
 // import data into database
