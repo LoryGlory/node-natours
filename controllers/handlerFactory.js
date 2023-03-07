20756cb3fce05ae366e40d7102fed449c826e4a8
Laura Roganovic <laura.roganovic@googlemail.com>
2023-03-07 20:43:54 +0100

    add update function

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -19,3 +19,27 @@ exports.deleteOne = (Model) =>
       data: null,
     });
   });
+
+exports.updateOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const document = await Model.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true,
+      }
+    );
+
+    if (!document) {
+      return next(
+        new AppError('No document found with that ID', 404)
+      );
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        document,
+      },
+    });
+  });
