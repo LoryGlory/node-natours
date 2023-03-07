b02a72af41a9152916d9199bfab0dd9f67431d12
Laura Roganovic <laura.roganovic@googlemail.com>
2023-03-07 13:03:52 +0100

    add review routes import and mount routes

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const hpp = require('hpp');
 const app = express();
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 
@@ -69,6 +70,7 @@ app.use((req, res, next) => {
 // middleware to mount routers
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter);
 
 // handle nonexistent routes
 app.all('*', (req, res, next) => {
