46eb4f376d09deebace895d8732b7d7d486676b0
Laura Roganovic <laura.roganovic@googlemail.com>
2023-03-08 13:10:12 +0100

    add autherization restriction to routes

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -27,17 +27,33 @@ router
 // return tour stats when route is /tour-stats
 router
   .route('/monthly-plan/:year')
-  .get(tourController.getMonthlyPlan);
+  .get(
+    authController.protect,
+    authController.restrictTo(
+      'admin',
+      'lead-guide',
+      'guide'
+    ),
+    tourController.getMonthlyPlan
+  );
 
 router
   .route('/')
-  .get(authController.protect, tourController.getAllTours)
-  .post(tourController.createOne);
+  .get(tourController.getAllTours)
+  .post(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide'),
+    tourController.createOne
+  );
 
 router
   .route('/:id')
   .get(tourController.getTour)
-  .patch(tourController.updateTour)
+  .patch(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide'),
+    tourController.updateTour
+  )
   .delete(
     authController.protect,
     authController.restrictTo('admin', 'lead-guide'),
