9c61f8753926b08430710aa1c01cbfc3791e786c
Laura Roganovic <laura.roganovic@googlemail.com>
2023-03-08 16:58:08 +0100

    add geospatial query to search for tours within a radius

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,6 +1,7 @@
 const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
+const AppError = require('../utils/appError');
 
 // default top tours sorted by highest average rating and price
 exports.aliasTopTours = (req, res, next) => {
@@ -102,6 +103,40 @@ exports.getMonthlyPlan = catchAsync(
       status: 'success',
       data: {
         plan,
+      },
+    });
+  }
+);
+
+// get tours within a radius
+exports.getToursWithin = catchAsync(
+  async (req, res, next) => {
+    const { distance, latlng, unit } = req.params;
+    const [lat, lng] = latlng.split(',');
+
+    const radius =
+      unit === 'mi' ? distance / 3963.2 : distance / 6378.1;
+
+    if (!lat || !lng) {
+      next(
+        new AppError(
+          'Please provide latitude and longitude in the format lat,lng.',
+          400
+        )
+      );
+    }
+
+    const tours = await Tour.find({
+      startLocation: {
+        $geoWithin: { $centerSphere: [[lng, lat], radius] },
+      },
+    });
+
+    res.status(200).json({
+      status: 'success',
+      results: tours.length,
+      data: {
+        data: tours,
       },
     });
   }
