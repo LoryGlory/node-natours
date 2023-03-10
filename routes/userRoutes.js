4836109a6036cbfd24d293ce24e06ef835bfb711
Laura Roganovic <laura.roganovic@googlemail.com>
2023-03-10 15:02:31 +0100

    add logout route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,6 +7,7 @@ const router = express.Router();
 // auth routes
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
+router.get('/logout', authController.logout);
 router.post(
   '/forgotPassword',
   authController.forgotPassword
