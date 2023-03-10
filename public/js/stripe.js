2ce49d8e47f2fe7d0542c541caec6215aa119e2f
Laura Roganovic <laura.roganovic@googlemail.com>
2023-03-10 23:46:18 +0100

    add axios checkout session

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -10,7 +10,7 @@ export const bookTour = async (tourId) => {
   try {
     // 1. Get checkout session from the API
     const session = await axios(
-      `/api/v1/bookings/checkout-session/${tourId}`
+      `/api/v1/bookings/checkout-session/${tourId}/`
     );
     console.log(session);
 
