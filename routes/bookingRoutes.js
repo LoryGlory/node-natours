ac91eb39f3105b0a8ad9782f00006855e8a4a784
Laura Roganovic <laura.roganovic@googlemail.com>
2023-03-10 22:20:11 +0100

    add route to checkout-session/:tourId

---
+++ routes/bookingRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bookingController = require('../controllers/bookingController');
+const authController = require('../controllers/authController');
+
+// add option to merge params, enabling parameters in other routers to merge
+const router = express.Router();
+
+router.get(
+  '/checkout-session/:tourId',
+  authController.protect,
+  bookingController.getCheckoutSession
+);
+
+module.exports = router;
