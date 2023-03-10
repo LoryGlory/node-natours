653d2da26557af26045b48f3f0928d4156f2cc7d
Laura Roganovic <laura.roganovic@googlemail.com>
2023-03-10 18:01:51 +0100

    add functions to upload and resize multiple images when updating tour

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,6 +2,41 @@ const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
 const AppError = require('../utils/appError');
+const multer = require('multer');
+const sharp = require('sharp');
+
+const multerStorage = multer.memoryStorage();
+
+// multer filter, allow only images
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true);
+  } else {
+    cb(
+      new AppError(
+        'Not an image! Please upload only images.',
+        400
+      ),
+      false
+    );
+  }
+};
+
+// multer config
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter,
+});
+
+exports.uploadTourImages = upload.fields([
+  { name: 'imageCover', maxCount: 1 },
+  { name: 'images', maxCount: 3 },
+]);
+
+exports.resizeTourImages = (req, res, next) => {
+  console.log(req.files);
+  next();
+};
 
 // default top tours sorted by highest average rating and price
 exports.aliasTopTours = (req, res, next) => {
