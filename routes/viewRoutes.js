295c1886a9df86ba6a0655f290400d0741d54f54
Laura Roganovic <laura.roganovic@googlemail.com>
2023-03-11 13:11:19 +0100

    add route to get booked tours for user

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -26,7 +26,11 @@ router.get(
   authController.protect,
   viewsController.getAccount
 );
-
+router.get(
+  '/my-tours',
+  authController.protect,
+  viewsController.getMyTours
+);
 router.post(
   '/submit-user-data',
   authController.protect,
